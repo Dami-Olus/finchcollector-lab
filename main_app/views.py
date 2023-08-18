01a6cd12975bcdcb99c0eeab7da91ed312f35799
Damola Olusakin <olusakinadedamola@gmail.com>
2023-08-17 23:07:18 -0400

    added Finch details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
-finches = [
-    {'species': 'House Finch', 'color': 'red', 'description': 'The house finch is a bird in the finch family Fringillidae. It is native to western North America and has been introduced to the eastern half of the continent and Hawaii.', 'spotted': 2 },
-    {'species': 'European Gold Finch', 'color': 'Gold', 'description': 'The European goldfinch or simply the goldfinch is a small passerine bird in the finch family that is native to Europe, North Africa and western and central Asia.', 'spotted': 4 },
-    {'species': 'Desert Finch', 'color': 'Sandy Brown', 'description': 'The desert finch, sometimes called Lichtensteins desert finch, is a large brown true finch found in southern Eurasia.', 'spotted': 2 },
-]
+from .models import Finch
+
+# finches = [
+#     {'species': 'House Finch', 'color': 'red', 'description': 'The house finch is a bird in the finch family Fringillidae. It is native to western North America and has been introduced to the eastern half of the continent and Hawaii.', 'spotted': 2 },
+#     {'species': 'European Gold Finch', 'color': 'Gold', 'description': 'The European goldfinch or simply the goldfinch is a small passerine bird in the finch family that is native to Europe, North Africa and western and central Asia.', 'spotted': 4 },
+#     {'species': 'Desert Finch', 'color': 'Sandy Brown', 'description': 'The desert finch, sometimes called Lichtensteins desert finch, is a large brown true finch found in southern Eurasia.', 'spotted': 2 },
+# ]
 
 # Create your views here.
 def home(request):
@@ -14,6 +16,13 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
+    })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch': finch
     })
