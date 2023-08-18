f3488c6d5c0557910f8f824f26cbeddb00f6b02e
Damola Olusakin <olusakinadedamola@gmail.com>
2023-08-17 22:25:28 -0400

    updated the string method of Finch model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    species = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    spotted = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.species} ({self.id})'
